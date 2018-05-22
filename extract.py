6ec88c40b0d9be3483949a3edcc7fcb55db4298e
s-ankur <33252312+s-ankur@users.noreply.github.com>
2018-05-22 15:25:48 +0530

    Add files via upload

--- extract.py
+++ extract.py
@@ -5,7 +5,12 @@ from random import random
 
 
 actions=glob.glob(os.path.join(VIDEO_PATH,'*'))
+try:
+    os.mkdir(IMAGE_PATH)
+except:pass
+
 for action in actions:
+    os.mkdir(action.replace(VIDEO_PATH,IMAGE_PATH))
     video_names=glob.glob('*')
     for video_name in video_names:
         video=Video(video_name)
