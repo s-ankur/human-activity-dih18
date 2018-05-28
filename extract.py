7efd82af24436924eb94e3612f355dc903f09248
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-05-29 00:58:15 +0530

    Working Now

--- extract.py
+++ extract.py
@@ -4,24 +4,27 @@ import os,glob
 from random import random
 
 
-action_names=glob.glob(os.path.join(VIDEO_PATH,'*'))
+category_names=glob.glob(os.path.join(VIDEO_PATH,'*'))
 try:
     os.mkdir(IMAGE_PATH)
 except:pass
 
-for action_name in action_names:
+for category_name in category_names:
     try:
-        os.mkdir(action_name.replace(VIDEO_PATH,IMAGE_PATH))
+        os.mkdir(category_name.replace(VIDEO_PATH,IMAGE_PATH))
     except:
         pass
-    video_names=glob.glob(os.path.join(action_name,'*'))
+    video_names=glob.glob(os.path.join(category_name,'*'))
     for video_name in video_names:
         video=Video(video_name)
         image_id=0
-        for frame in video:
-            if CHANCE>random():
-                image_name=os.path.join(action_name.replace(VIDEO_PATH,IMAGE_PATH),str(image_id)+'.jpg')
-                print(image_name)
-                imwrite(image_name,frame)
-                image_id+=1
-                
+        try:
+            for frame in video:
+                if CHANCE>random():
+                    image_name=os.path.join(category_name.replace(VIDEO_PATH,IMAGE_PATH),str(image_id)+'.jpg')
+                    print(image_name)
+                    imwrite(image_name,frame)
+                    image_id+=1
+        except:
+            pass
+                    
