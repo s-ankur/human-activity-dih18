3d3a7ddc6ed17df59c15bfe0462cb620f2f84f66
rastogip2011 <35422675+rastogip2011@users.noreply.github.com>
2018-06-02 15:24:35 +0530

    Add files via upload

--- extract.py
+++ extract.py
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 from utility.cv_utils import *
 from config import *
 import os,glob
@@ -14,10 +21,10 @@ for category_name in category_names:
         os.mkdir(category_name.replace(VIDEO_PATH,IMAGE_PATH))
     except:
         pass
+    image_id=0
     video_names=glob.glob(os.path.join(category_name,'*'))
     for video_name in video_names:
         video=Video(video_name)
-        image_id=0
         try:
             for frame in video:
                 if CHANCE>random():
