c87d10d69567a475be17b0ed1c57aa498ae17d95
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-06-02 15:04:27 +0530

    3d cnn added

--- extract.py
+++ extract.py
@@ -1,30 +1,26 @@
 from utility.cv_utils import *
 from config import *
-import os,glob
+import os
 from random import random
 
-
-category_names=glob.glob(os.path.join(VIDEO_PATH,'*'))
-try:
-    os.mkdir(IMAGE_PATH)
-except:pass
-
-for category_name in category_names:
-    try:
-        os.mkdir(category_name.replace(VIDEO_PATH,IMAGE_PATH))
-    except:
-        pass
-    video_names=glob.glob(os.path.join(category_name,'*'))
-    for video_name in video_names:
-        video=Video(video_name)
+def extract():
+    category_names=os.listdir(VIDEO_PATH)
+    if not os.path.isdir(IMAGE_PATH)
+        os.mkdir(IMAGE_PATH)
+    for category_name in category_names:
+        source_directory = os.path.join(VIDEO_PATH,category_name)
+        destination_directory = os.path.join(IMAGE_PATH,category_name)
+        video_paths=listdirp(source_directory)
+        if not os.path.isdir(destination_directory):
+            os.mkdir(destination_directory)
         image_id=0
-        try:
+        for video_path in video_paths:
+            video=Video(video_path)            
             for frame in video:
                 if CHANCE>random():
-                    image_name=os.path.join(category_name.replace(VIDEO_PATH,IMAGE_PATH),str(image_id)+'.jpg')
-                    print(image_name)
-                    imwrite(image_name,frame)
+                    image_path=os.path.join(destination_directory,str(image_id)+'.jpg')
+                    print(image_path)
+                    imwrite(image_path,frame)
                     image_id+=1
-        except:
-            pass
-                    
+
+
