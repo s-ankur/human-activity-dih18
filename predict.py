01c936522d0f458a300a8e72572d937d0e414b85
rastogip2011 <35422675+rastogip2011@users.noreply.github.com>
2018-06-03 23:38:56 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,9 +1,15 @@
 from config import *
 from model import * 
+from utility.cv_utils import *
 import sys
 import numpy as np
-X_test=load_images(sys.argv[1])
-y_pred=model.predict(X_test)
-y_pred = np.argmax(1)
-for pred in y_pred:
-    print(actions[pred])
+video=Video(sys.argv[1])
+for frame in video:
+	frame=cv2.resize(frame,SIZE)
+	cv2.imshow('window',frame)
+	cv2.waitKey(1)
+	prediction=model.predict(frame.reshape((1,*frame.shape)))
+	#print(prediction)
+	index=np.argmax(prediction)
+	print(categories[index])
+destroy_window('window')
