74950766396483f42b40b18ff842a713807dc2d3
rastogip2011 <35422675+rastogip2011@users.noreply.github.com>
2018-06-13 12:40:45 +0530

    Add files via upload

--- transform.py
+++ transform.py
@@ -1,6 +1,7 @@
 import dtcwt
 import dtcwt.registration as registration
 import numpy as np
+import matplotlib.pyplot as plt
 
 transform2d = dtcwt.Transform2d()
 
@@ -15,8 +16,12 @@ transform2d = dtcwt.Transform2d()
 
 #quiver(X[::step,::step], Y[::step,::step],vxs[::step,::step], vys[::step,::step],color='g', angles='xy', scale_units='xy', scale=0.25)
 
-
-
+from utility.cv_utils import *
+import sys
+a=imread(sys.argv[1])
+b=imread(sys.argv[2])
+a=a[:,:,1]
+b=b[:,:,1]
 
 def transform_dtcwt(ref,src):
 	ref_t = transform2d.forward(ref, nlevels=6)
@@ -27,3 +32,21 @@ def transform_dtcwt(ref,src):
 	vys = vys*ref.shape[0]
 	mesh=np.abs(vxs + 1j*vys)
 	return mesh
+
+c = transform_dtcwt(a,b)
+
+c[c< c.max()*.2]=0
+d=plt.imshow(c,cmap='hot')
+
+plt.colorbar(d)
+plt.figure()
+k=np.dstack([a,b,c*(255/7)])
+plt.imshow(k.astype('int'))
+plt.figure()
+plt.imshow(a)
+plt.figure()
+plt.imshow(b)
+plt.show()
+
+
+
