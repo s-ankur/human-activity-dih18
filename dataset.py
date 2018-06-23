239754d1cfb741acd8bed149f63cea7f78860af7
prashant.rastogi.eee16 <prashant.rastogi.eee16@itbhu.ac.in>
2018-06-23 14:42:11 +0530

    cnn3d completed

--- dataset.py
+++ dataset.py
@@ -17,20 +17,3 @@ def load_data(categories,size=SIZE,mode=MODE):
     X_train=np.array(training_data)
     y_train=np.array(training_labels)
     return X_train, y_train
-
-def load_data3d(categories,size=SIZE,mode=MODE):
-    training_data = []
-    training_labels = []
-    for label,category in enumerate(categories):
-        files = glob.glob(os.path.join(category,'*'))
-        print("Category %s --- %d files"%(category,len(files)))
-        for file in files:
-            image = imread(file,mode)
-            image = cv2.resize(image,size)
-            training_data.append(image)  
-            training_labels.append(label)
-    X_train=np.array(training_data)
-    y_train=np.array(training_labels)
-    return X_train, y_train
-
-
---
+++ model3d.py
@@ -0,0 +1,22 @@
+from config3d import *
+from cnn3d import cnn3d_model
+import glob,os
+
+categories=glob.glob(os.path.join(CLIP_PATH,'*'))
+model = cnn3d_model(input_shape=SIZE3D+(3,3),num_classes=len(categories))
+model.compile(optimizer='adam', loss='categorical_crossentropy' )
+
+def load_model():
+    model.load_weights(MODEL_NAME)
+    print ('Loaded model successfully')
+
+def save_model():
+    model.save_weights(MODEL_NAME)
+    print ('Saved model successfully')
+    
+
+try:
+    load_model()
+except:
+    pass
+    
