a81573be684c459d588774265a5ad0777019534a
lucifer2278 <36075292+lucifer2278@users.noreply.github.com>
2018-06-28 23:24:08 +0530

    Create visualize.py

---
+++ visualize.py
@@ -0,0 +1,10 @@
+from keras.utils.vis_utils import plot_model
+import sys
+
+
+
+if  sys.argv[1]=='3d':
+  from model3d import *
+else:
+  from model import *
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
