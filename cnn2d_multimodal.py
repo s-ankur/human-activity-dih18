adaaac6002ae5468deaee81f5e72a189c0f824dc
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-07-01 17:08:45 +0530

    functional

--- cnn2d_multimodal.py
+++ cnn2d_multimodal.py
@@ -1,32 +1,31 @@
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Activation, Conv2D, GlobalAveragePooling2D, BatchNormalization, SeparableConv2D, MaxPooling2D
-from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Input, Activation, Conv2D, BatchNormalization, MaxPooling2D
+from keras.models import Model
 
 
 def cnn2d_model(input_shape, num_classes):
-    model = Sequential()
+    x = Input(input_shape)
+    x = Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(32, 3, 3)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
 
-    model.add(Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(32, 3, 3))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    x = Conv2D(64, 3, 3, border_mode='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(64, 3, 3)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
 
-    model.add(Conv2D(64, 3, 3, border_mode='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(64, 3, 3))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    x = Flatten()(x)
+    x = Dense(512)(x)
+    x = Activation('relu')(x)
+    x = Dropout(0.6)(x)
+    x = Dense(num_classes)(x)
+    x = Activation('softmax')(x)
 
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.6))
-    model.add(Dense(num_classes))
-    model.add(Activation('softmax'))
-
+    model = Model(input_shape, x)
     return model
