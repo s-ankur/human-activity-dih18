573cb66f684408e73a899c53f0d649b4eb74f004
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-07-03 00:58:47 +0530

    visualizer and dummy classifier

---
+++ dummy.py
@@ -0,0 +1,3 @@
+from sklearn.dummy import DummyClassifier
+
+cnn2d_model=cnn3d_model=lambda *args :Dummy_classifier(strategy='most_frequent')
--- visualize.py
+++ visualize.py
@@ -1,19 +1,15 @@
 from vis.losses import ActivationMaximization
 from vis.regularizers import TotalVariation, LPNorm
-from vis.modifiers import Jitter
 from vis.optimizer import Optimizer
 
 from vis.callbacks import GifGenerator
-from vis.utils.vggnet import VGG16
-
-# Build the VGG16 network with ImageNet weights
-model = VGG16(weights='imagenet', include_top=True)
-print('Model loaded.')
+from model3d import model
 
 # The name of the layer we want to visualize
 # (see model definition in vggnet.py)
-layer_name = 'predictions'
+layer_name = 'dense_2'
 layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
+
 output_class = [20]
 
 losses = [
@@ -21,6 +17,5 @@ losses = [
     (LPNorm(model.input), 10),
     (TotalVariation(model.input), 10)
 ]
-opt = Optimizer(model.input, losses)
-opt.minimize(max_iter=500, verbose=True, image_modifiers=[Jitter()], callbacks=[GifGenerator('opt_progress')])
- 
+#opt = Optimizer(model.input, losses)
+#opt.minimize(max_iter=500, verbose=True, callbacks=[GifGenerator('opt_progress')])
