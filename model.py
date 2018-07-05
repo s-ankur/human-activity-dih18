f31e94078a686ed69cb3a9b95b9ef3adb20299a2
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-07-05 17:12:47 +0530

    model import system changed

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import glob
 import os
 
 categories = glob.glob(os.path.join(IMAGE_PATH, '*'))
-cnn2d_model = __import__(MODEL, fromlist=['cnn3d_model'])
+cnn2d_model = __import__(MODEL).cnn2d_model
 model = cnn2d_model(input_shape=SIZE + (3,), num_classes=len(categories))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
--- model3d.py
+++ model3d.py
@@ -3,7 +3,7 @@ import glob
 import os
 
 categories = glob.glob(os.path.join(CLIP_PATH, '*'))
-cnn3d_model = __import__(MODEL, fromlist=['cnn3d_model'])
+cnn3d_model = __import__(MODEL).cnn3d_model
 model = cnn3d_model(input_shape=SIZE3D + (DEPTH, 3), num_classes=len(categories))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
