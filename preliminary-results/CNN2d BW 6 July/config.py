1f0c8bf42c43b6c5c9100f700b4b147c217edf7b
s-ankur <sankur.shrikant.eee16@itbhu.ac.in>
2018-07-07 17:57:48 +0530

    added results

---
+++ preliminary-results/CNN2d BW 6 July/config.py
@@ -0,0 +1,16 @@
+############### CONFIG ##############
+
+VIDEO_PATH = 'videos'
+IMAGE_PATH = 'images'
+RESULT_PATH = 'results'
+MODEL_NAME = 'saved'
+MODEL = 'cnn2d'
+
+CHANCE = .1
+TEST_TRAIN_SPLIT = .33
+SIZE = (64, 64)
+CHANNELS = 1
+BATCH_SIZE = 128
+EPOCHS = 30
+
+############### CONFIG ##############
